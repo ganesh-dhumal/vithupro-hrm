75d34c624c96a3b4632a7a30bb8cdafe03f93f8c
Ganesh Dhumal <116799801+ganesh-dhumal@users.noreply.github.com>
2025-08-12 11:23:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return "Hello, World! Deployed via Docker + CI/CD on Local 🚀"
+    return "Hello, World! Deployed via Docker + CI/CD on Local for testing 🚀"
 
 if __name__ == "__main__":
     # For local dev only; in Docker we’ll run gunicorn
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=5000)
 
